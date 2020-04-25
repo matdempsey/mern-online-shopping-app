d673b56c86b38bb757e0d442d932aead0a650122
Mathew <mat.dempsey27@gmail.com>
2020-04-25 14:39:36 +0100

    useState hooks for first and last name, handle functions, new server folder w/ gitignore

--- client/src/components/create-account/CreateAccount.js
+++ client/src/components/create-account/CreateAccount.js
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form, FormGroup, Label, Input } from "reactstrap";
 import { withRouter } from "react-router-dom";
 
 const CreateAccount = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const handleFirstNameChange = (e) => {
+    setFirstName(e.target.value);
+    console.log(firstName);
+  };
+
+  const handleLastNameChange = (e) => {
+    setLastName(e.target.value);
+    console.log(lastName);
+  };
+
+  const handleEmailChange = (e) => {
+    // setter
+  };
+  const handlePasswordChange = (e) => {
+    // setter
+  };
+
+  const handleConfirmPasswordChange = (e) => {
+    // setter
+  };
+
   const onSubmit = () => {
     console.log("Submit button clicked");
     //  validate fields
     //  submit to db
+    console.log(`${firstName} ${lastName}`);
+  };
+
+  const user = {
+    fname: firstName,
+    lname: lastName,
   };
 
   return (
@@ -14,21 +44,21 @@ const CreateAccount = () => {
       <Form>
         <FormGroup>
           <Label>First Name</Label>
-          <Input type="text" />
-          <Label>Surname</Label>
-          <Input type="text" />
+          <Input type="text" onChange={handleFirstNameChange} />
+          <Label>Last Name</Label>
+          <Input type="text" onChange={handleLastNameChange} />
         </FormGroup>
         <FormGroup>
           <Label>Email</Label>
-          <Input type="email" />
+          <Input type="email" onChange={handleEmailChange} />
         </FormGroup>
         <FormGroup>
           <Label>Password</Label>
-          <Input type="password" />
+          <Input type="password" onChange={handlePasswordChange} />
         </FormGroup>
         <FormGroup>
           <Label>Confirm Password</Label>
-          <Input type="password" />
+          <Input type="password" onChange={handlePasswordChange} />
         </FormGroup>
         <FormGroup>
           <Button color="primary" onClick={onSubmit}>
