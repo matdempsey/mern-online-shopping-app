f4f62aef199b9096b41665b23ad2a98f3b5ae1db
Mathew Dempsey <mat.dempsey27@gmail.com>
2020-05-09 13:56:32 +0100

    WIP: log in process

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,11 +1,32 @@
-import React from "react";
-import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import React, { useState } from "react";
+import { Alert, Button, Form, FormGroup, Label, Input } from "reactstrap";
 import { withRouter } from "react-router-dom";
 
 const Login = (props) => {
   const { history } = props;
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [showError, setShowError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
   const onLoginClick = () => {
-    console.log("Login button clicked");
+    if (email === "" || password === "") {
+      // set state to true for error component
+      setShowError(true);
+      setErrorMessage("Please enter your email address and password.");
+    }
+
+    fetch("");
   };
 
   const onCreateAccountClick = () => {
@@ -18,11 +39,24 @@ const Login = (props) => {
       <Form>
         <FormGroup>
           <Label>Email</Label>
-          <Input type="email" placeholder="Email Address" />
+          <Input
+            type="email"
+            onChange={handleEmailChange}
+            placeholder="Email Address"
+          />
         </FormGroup>
         <FormGroup>
           <Label>password</Label>
-          <Input type="password" placeholder="Password" />
+          <Input
+            type="password"
+            onChange={handlePasswordChange}
+            placeholder="Password"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Alert color="danger" isOpen={showError}>
+            {errorMessage}
+          </Alert>
         </FormGroup>
         <FormGroup>
           <Button color="primary" onClick={onLoginClick}>
