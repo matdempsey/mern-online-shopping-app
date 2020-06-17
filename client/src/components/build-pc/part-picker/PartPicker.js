6a4609c13fdd417b1ed1fb180f77358cc6e166af
Mathew Dempsey <mat.dempsey27@gmail.com>
2020-06-17 15:45:10 +0100

    temp fix: removed loadingData state in PartPicker as dropdown not rendering if no server data

--- client/src/components/build-pc/part-picker/PartPicker.js
+++ client/src/components/build-pc/part-picker/PartPicker.js
@@ -5,7 +5,6 @@ import DropdownList from "../../dropdown-list/DropdownList.js";
 
 const PartPicker = () => {
   const [componentList, setComponentList] = useState([]);
-  const [loadingData, setLoadingData] = useState(true);
 
   useEffect(() => {
     fetch("/api/components", {
@@ -17,7 +16,6 @@ const PartPicker = () => {
       .then((res) => res.json())
       .then((res) => {
         setComponentList(res);
-        setLoadingData(false);
       });
   }, []);
 
@@ -28,42 +26,40 @@ const PartPicker = () => {
   };
 
   return (
-    !loadingData && (
-      <>
-        <div>
-          <Form>
-            <FormGroup>
-              <Label size="lg">Motherboard</Label>
-              <DropdownList content={filterComponents("Motherboard")} />
-            </FormGroup>
-            <FormGroup>
-              <Label size="lg">Processor</Label>
-              <DropdownList content={filterComponents("Processor")} />
-            </FormGroup>
-            <FormGroup>
-              <Label size="lg">Memory</Label>
-              <DropdownList content={filterComponents("Memory")} />
-            </FormGroup>
-            <FormGroup>
-              <Label size="lg">Graphics Card</Label>
-              <DropdownList content={filterComponents("Graphics Card")} />
-            </FormGroup>
-            <FormGroup>
-              <Label size="lg">Storage</Label>
-              <DropdownList content={filterComponents("Storage")} />
-            </FormGroup>
-            <FormGroup>
-              <Label size="lg">Power Supply</Label>
-              <DropdownList content={filterComponents("Power Supply")} />
-            </FormGroup>
-            <FormGroup>
-              <Label size="lg">Operating System</Label>
-              <DropdownList content={filterComponents("Operating System")} />
-            </FormGroup>
-          </Form>
-        </div>
-      </>
-    )
+    <>
+      <div>
+        <Form>
+          <FormGroup>
+            <Label size="lg">Motherboard</Label>
+            <DropdownList content={filterComponents("Motherboard")} />
+          </FormGroup>
+          <FormGroup>
+            <Label size="lg">Processor</Label>
+            <DropdownList content={filterComponents("Processor")} />
+          </FormGroup>
+          <FormGroup>
+            <Label size="lg">Memory</Label>
+            <DropdownList content={filterComponents("Memory")} />
+          </FormGroup>
+          <FormGroup>
+            <Label size="lg">Graphics Card</Label>
+            <DropdownList content={filterComponents("Graphics Card")} />
+          </FormGroup>
+          <FormGroup>
+            <Label size="lg">Storage</Label>
+            <DropdownList content={filterComponents("Storage")} />
+          </FormGroup>
+          <FormGroup>
+            <Label size="lg">Power Supply</Label>
+            <DropdownList content={filterComponents("Power Supply")} />
+          </FormGroup>
+          <FormGroup>
+            <Label size="lg">Operating System</Label>
+            <DropdownList content={filterComponents("Operating System")} />
+          </FormGroup>
+        </Form>
+      </div>
+    </>
   );
 };
 
