33c87fc96069ab1e100771fe3299f607f6793325
Mathew Dempsey <mat.dempsey27@gmail.com>
2020-11-24 23:42:33 +0000

    Removed redundant TableRow, moved code back into Table for now

--- client/src/components/table/Table.js
+++ client/src/components/table/Table.js
@@ -1,14 +1,74 @@
 import React from "react";
-import { Container } from "reactstrap";
-import TableRow from "./TableRow";
+import { Container, Label, Row, Col } from "reactstrap";
+
+import "./Table.css";
 
 const Table = (props) => {
   const { caseObj } = props;
+  const {
+    width,
+    depth,
+    height,
+    formFactor,
+    maxGraphicsCardLength,
+    description,
+    suitability,
+    price,
+  } = caseObj;
 
   return (
     <div>
       <Container>
-        <TableRow caseObj={caseObj} />
+        <div>
+          <Row>
+            <Col className="key-col">
+              <Label>W x D x H</Label>
+            </Col>
+            <Col>
+              <Label>{`${width} x ${depth} x ${height}`}</Label>
+            </Col>
+          </Row>
+          <Row>
+            <Col className="key-col">
+              <Label>Form Factor</Label>
+            </Col>
+            <Col>
+              <Label>{formFactor}</Label>
+            </Col>
+          </Row>
+          <Row>
+            <Col className="key-col">
+              <Label>Max Graphics Card Length</Label>
+            </Col>
+            <Col>
+              <Label>{maxGraphicsCardLength}</Label>
+            </Col>
+          </Row>
+          <Row>
+            <Col className="key-col">
+              <Label>Description</Label>
+            </Col>
+            <Col>
+              <Label>{description}</Label>
+            </Col>
+          </Row>
+          <Row>
+            <Col className="key-col">
+              <Label>Suitability</Label>
+            </Col>
+            <Col>
+              <Label>{suitability}</Label>
+            </Col>
+          </Row>
+          <Row>
+            <Col className="key-col">
+              <Label>Price</Label>
+            </Col>
+            <Col>
+              <Label>£{price}</Label>
+            </Col>
+          </Row>
+        </div>
       </Container>
     </div>
   );
