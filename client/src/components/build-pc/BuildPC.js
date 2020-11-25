84672db3219373e17adc456c95ddb1d237385e01
Mathew Dempsey <mat.dempsey27@gmail.com>
2020-11-25 17:05:09 +0000

    adjustments to BuildPC layout

--- client/src/components/build-pc/BuildPC.js
+++ client/src/components/build-pc/BuildPC.js
@@ -6,6 +6,7 @@ import NavBar from "../common/nav-bar/NavBar.js";
 import CasePicker from "./case-picker/CasePicker.js";
 import ComponentPicker from "./component-picker/ComponentPicker.js";
 import Summary from "./summary/Summary.js";
+import Footer from "../footer/Footer.js";
 
 import "./BuildPC.css";
 
@@ -26,23 +27,37 @@ const BuildPC = () => {
     <>
       <HeaderBar />
       <NavBar />
-      <Label className="section-banner" size="lg">
-        1) SELECT A CASE
-      </Label>
-      <CasePicker selectedCaseCostFunc={handleSelectedCaseCost} />
-      <Label className="section-banner" size="lg">
-        2) CHOOSE YOUR CORE COMPONENTS
-      </Label>
-      <ComponentPicker
-        selectedComponentCostFunc={handleSelectedComponentCost}
-      />
-      <Label className="section-banner" size="lg">
-        3) REVIEW COST
-      </Label>
-      <Summary caseCost={caseCost} componentCost={componentCost} />
-      <div className="button-pos">
-        <Button color="success">Continue</Button>
+      <div className="flex-build-pc-parent-container">
+        <div className="build-pc-container">
+          <Label className="section-banner" size="lg">
+            1) SELECT A CASE
+          </Label>
+
+          <CasePicker selectedCaseCostFunc={handleSelectedCaseCost} />
+
+          <Label className="section-banner" size="lg">
+            2) CHOOSE YOUR CORE COMPONENTS
+          </Label>
+
+          <div className="section-padding">
+            <ComponentPicker
+              selectedComponentCostFunc={handleSelectedComponentCost}
+            />
+          </div>
+
+          <Label className="section-banner" size="lg">
+            3) REVIEW COST
+          </Label>
+
+          <div className="section-padding">
+            <Summary caseCost={caseCost} componentCost={componentCost} />
+          </div>
+          <div className="button-pos">
+            <Button color="success">Continue</Button>
+          </div>
+        </div>
       </div>
+      <Footer />
     </>
   );
 };
