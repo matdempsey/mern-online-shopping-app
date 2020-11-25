26f66dc7df8d23dcf1bdc3fb8ae9078e2ac2c07a
Mathew Dempsey <mat.dempsey27@gmail.com>
2020-11-25 16:03:45 +0000

    new icons for log-in and basket

--- client/src/components/header-bar/HeaderBar.js
+++ client/src/components/header-bar/HeaderBar.js
@@ -3,6 +3,7 @@ import { withRouter, Link } from "react-router-dom";
 import CompanyLogo from "../common/company-logo/CompanyLogo.js";
 import SearchBar from "../common/search-bar/SearchBar.js";
 import basket from "./../../images/basket.png";
+import logIn from "./../../images/log-in.png";
 
 import "./HeaderBar.css";
 
@@ -21,24 +22,34 @@ const HeaderBar = (props) => {
 
   return (
     <>
-      <div class="flex-hb-container">
+      <div className="flex-hb-container">
         <div className="hb-company-logo-container">
           <CompanyLogo />
         </div>
+
         <div className="sb-container">
           <SearchBar />
         </div>
+
         <div className="flex-hb-container-right">
-          <Link className="hb-link" to={"/login"}>
-            log-in/create account
-          </Link>
-          <img
-            className="basket"
-            src={basket}
-            alt="your basket"
-            onClick={handleOnBasketClick}
-          ></img>
-          <span className="basket-count"> 0</span>
+          <div className="basket-container">
+            <Link className="hb-link" to={"/basket"}>
+              <img
+                className="basket-icon"
+                src={basket}
+                alt="your basket"
+                onClick={handleOnBasketClick}
+              />
+              basket
+            </Link>
+          </div>
+
+          <div className="log-in-container">
+            <Link className="hb-link" to={"/login"}>
+              <img className="log-in-icon" src={logIn} alt="log in" />
+              log-in
+            </Link>
+          </div>
         </div>
       </div>
     </>
