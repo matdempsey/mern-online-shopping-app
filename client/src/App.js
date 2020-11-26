1c9bae866c3ba88ee8fa5495eba540d36bede920
Mathew Dempsey <mat.dempsey27@gmail.com>
2020-11-26 21:55:25 +0000

    .

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Home from "./components/home/Home";
 import Login from "./components/login/Login.js";
 import CreateAccount from "./components/create-account/CreateAccount.js";
 import BuildPC from "./components/build-pc/BuildPC.js";
-import Search from "./components/search/Search.js";
+import SearchResults from "./components/search/SearchResults.js";
 import PageNotFound from "./components/page-not-found/PageNotFound.js";
 
 import "./App.css";
@@ -15,12 +15,12 @@ const App = () => {
       <div className="main-container">
         <Router>
           <Switch>
-            <Route exact path="/" component={Home}></Route>
-            <Route path="/login" component={Login}></Route>
-            <Route path="/create-account" component={CreateAccount}></Route>
-            <Route path="/build-pc" component={BuildPC}></Route>
-            <Route path="/search" component={Search}></Route>
-            <Route component={PageNotFound}></Route>
+            <Route exact path="/" component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/create-account" component={CreateAccount} />
+            <Route path="/build-pc" component={BuildPC} />
+            <Route path="/search" component={SearchResults} />
+            <Route component={PageNotFound} />
           </Switch>
         </Router>
       </div>
