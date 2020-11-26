d8931510598fed3ece333476e4c13f3cbbf2a2a7
Mathew Dempsey <mat.dempsey27@gmail.com>
2020-11-26 21:37:28 +0000

    removed text in footer

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -3,11 +3,7 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
-  return (
-    <div id="footer-container">
-      <p>this is the footer</p>
-    </div>
-  );
+  return <div id="footer-container"></div>;
 };
 
 export default Footer;
