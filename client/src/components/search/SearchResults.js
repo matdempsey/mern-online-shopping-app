b67fa7361a1c826180382baebf7392f1e5ff6120
Mathew Dempsey <mat.dempsey27@gmail.com>
2020-12-01 17:10:46 +0000

    can now handle search for products manually via URL

--- client/src/components/search/SearchResults.js
+++ client/src/components/search/SearchResults.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import HeaderBar from "../header-bar/HeaderBar.js";
 import NavBar from "../common/nav-bar/NavBar.js";
 import SearchResultsCount from "./SearchResultsCount.js";
@@ -7,9 +7,27 @@ import Footer from "../footer/Footer.js";
 
 const SearchResults = (props) => {
   const { location } = props;
-  const { searchText, results } = location.state;
+  const [searchText, setSearchText] = useState("");
+  const [results, setResults] = useState([]);
 
-  console.log("search text = ", searchText);
+  // handles user manually entering in the url (i.e. /search?q=someValue) where location.state will be undefined -- causing crash.
+  useEffect(() => {
+    // user has manually inputted
+    if (location.state === undefined) {
+      const queryString = new URLSearchParams(location.search);
+      const parsedSearchText = queryString.get("q");
+      setSearchText(parsedSearchText);
+
+      fetch(`/api/search/${location.search}`)
+        .then((res) => res.json())
+        .then((res) => setResults(res))
+        .catch((err) => console.log(err));
+    } else {
+      const { searchText, results } = location.state;
+      setSearchText(searchText);
+      setResults(results);
+    }
+  }, [location.key]);
 
   return (
     <div>
