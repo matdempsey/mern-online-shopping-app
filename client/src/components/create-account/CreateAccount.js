4bbed3426d69efc0c2c6f5e3dcfdec6dc894fc8f
Mathew Dempsey <mat.dempsey27@gmail.com>
2020-12-12 18:33:51 +0000

    reduced customer states into one single state by using an object instead

--- client/src/components/create-account/CreateAccount.js
+++ client/src/components/create-account/CreateAccount.js
@@ -6,38 +6,32 @@ import { Link } from "react-router-dom";
 import "./CreateAccount.css";
 
 const CreateAccount = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const [customer, setCustomer] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
 
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [showError, setShowError] = useState(false);
   const [errorMessage, setErrorMessage] = useState([]);
 
   let errorMsgArr = [];
 
-  const customerDetails = {
-    firstName: firstName,
-    lastName: lastName,
-    email: email,
-    password: password,
-    confirmPassword: confirmPassword,
-  };
-
   const handleFirstNameChange = (e) => {
-    setFirstName(e.target.value);
+    setCustomer({ ...customer, firstName: e.target.value });
   };
 
   const handleLastNameChange = (e) => {
-    setLastName(e.target.value);
+    setCustomer({ ...customer, lastName: e.target.value });
   };
 
   const handleEmailChange = (e) => {
-    setEmail(e.target.value);
+    setCustomer({ ...customer, email: e.target.value });
   };
   const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
+    setCustomer({ ...customer, password: e.target.value });
   };
 
   const handleConfirmPasswordChange = (e) => {
@@ -46,52 +40,52 @@ const CreateAccount = () => {
 
   const onSubmit = () => {
     if (
-      firstName === "" &&
-      lastName === "" &&
-      email === "" &&
-      password === ""
+      customer.firstName === "" &&
+      customer.lastName === "" &&
+      customer.email === "" &&
+      customer.password === ""
     ) {
       errorMsgArr.push("Please fill in all fields.");
     } else {
-      if (firstName.trim() === "") {
+      if (customer.firstName.trim() === "") {
         errorMsgArr.push("Please enter your first name.");
       }
 
-      if (lastName.trim() === "") {
+      if (customer.lastName.trim() === "") {
         errorMsgArr.push("Please enter your last name.");
       }
 
-      if (email.trim() === "") {
+      if (customer.email.trim() === "") {
         errorMsgArr.push("Please enter your email address.");
       } else {
         //check if there is at least one . after @
         const charReg = /[@]/;
-        if (email.search(charReg) === -1)
+        if (customer.email.search(charReg) === -1)
           errorMsgArr.push("Invalid email address.");
       }
 
       //password validation block
-      if (password.trim() === "") {
+      if (customer.password.trim() === "") {
         errorMsgArr.push("Please enter your password.");
       } else {
         const capsReg = /[A-Z]/;
         const numReg = /[\d]+/;
 
-        if (password.search(capsReg) === -1) {
+        if (customer.password.search(capsReg) === -1) {
           errorMsgArr.push(
             "Password must contain at least one capital letter."
           );
         }
 
-        if (password.search(numReg) === -1) {
+        if (customer.password.search(numReg) === -1) {
           errorMsgArr.push("Password must contain at least one number.");
         }
 
-        if (password.length < 8) {
+        if (customer.password.length < 8) {
           errorMsgArr.push("Password must be at least eight characters long.");
         }
 
-        if (password !== confirmPassword) {
+        if (customer.password !== confirmPassword) {
           errorMsgArr.push("Passwords don't match. Please try again.");
         }
       }
@@ -117,15 +111,8 @@ const CreateAccount = () => {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(customerDetails),
-    })
-      .then((res) => res.json())
-      .then(() => {
-        setErrorMessage(
-          errorMsgArr.push("An account with this email address already exists.")
-        );
-      })
-      .catch((e) => console.log("error:", e.message));
+      body: JSON.stringify(customer),
+    });
   };
 
   return (
@@ -137,9 +124,7 @@ const CreateAccount = () => {
         <div className="ca-form">
           <Form>
             <FormGroup>
-              <Label className="ca-title" size="lg">
-                Create account
-              </Label>
+              <Label className="ca-title">Create Account</Label>
               <Label>First Name</Label>
               <Input type="text" onChange={handleFirstNameChange} />
               <Label>Last Name</Label>
@@ -162,7 +147,16 @@ const CreateAccount = () => {
               <Alert color="danger" isOpen={showError}>
                 <ul>
                   {errorMessage.map((ele) => (
-                    <li>{ele}</li>
+                    <li
+                      className={
+                        errorMessage.length > 1
+                          ? "error-li"
+                          : "error-li-no-style"
+                      }
+                      key={ele}
+                    >
+                      {ele}
+                    </li>
                   ))}
                 </ul>
               </Alert>
