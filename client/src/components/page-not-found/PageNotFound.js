e21d39657e11c8f67dad2afa681f492258102917
Mathew Dempsey <mat.dempsey27@gmail.com>
2020-12-30 14:03:38 +0000

    removed emoji

--- client/src/components/page-not-found/PageNotFound.js
+++ client/src/components/page-not-found/PageNotFound.js
@@ -8,14 +8,9 @@ const PageNotFound = () => {
     <div>
       <div className="page-not-found-container">
         <h1 className="page-not-found-heading">404</h1>
-        <h2 className="page-not-found-sub-heading">
-          Page not found
-          <span role="img" aria-label="pensive face">
-            😔
-          </span>
-        </h2>
+        <h2 className="page-not-found-sub-heading">Page not found</h2>
         <p className="page-not-found-error-msg">
-          Sorry, we were unable to find the page you were looking for..
+          Sorry, we were unable to find the page you were looking for.
         </p>
         <Link to="/">Back to homepage</Link>
       </div>
