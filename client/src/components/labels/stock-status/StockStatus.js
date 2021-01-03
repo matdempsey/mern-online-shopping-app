b1d15165aceeaa9687fb2f6532b931a56715691d
Mathew Dempsey <mat.dempsey27@gmail.com>
2021-01-03 15:23:07 +0000

    stock status for products now just text and has been enlarged

--- client/src/components/labels/stock-status/StockStatus.js
+++ client/src/components/labels/stock-status/StockStatus.js
@@ -1,6 +1,4 @@
 import React from "react";
-import inStockImage from "../../../images/stock-status/in-stock.png";
-import outOfStockImage from "../../../images/stock-status/out-of-stock.png";
 
 import "./StockStatus.css";
 
@@ -10,15 +8,9 @@ const StockStatus = (props) => {
   return (
     <div>
       {qty > 0 ? (
-        <span>
-          <img className="stock-status-img" src={inStockImage}></img>
-          <span className="in-stock-status-text">in stock</span>
-        </span>
+        <span className="in-stock-status-text">in stock</span>
       ) : (
-        <span>
-          <img className="stock-status-img" src={outOfStockImage}></img>
-          <span className="out-of-stock-status-text">out of stock</span>
-        </span>
+        <span className="out-of-stock-status-text">out of stock</span>
       )}
     </div>
   );
