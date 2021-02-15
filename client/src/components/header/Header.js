d12fd8f3a283625706396e9f369f9712c3cd6f16
Mathew Dempsey <mat.dempsey27@gmail.com>
2021-02-15 17:24:00 +0000

    removed "currentUser" from local storage after logout

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -38,6 +38,7 @@ const Header = (props) => {
     if (isAuthenticated) {
       fetch("/api/logout").then(() => {
         localStorage.removeItem("_sid");
+        localStorage.removeItem("currentUser");
         setIsAuthenticated(false);
       });
     } else {
